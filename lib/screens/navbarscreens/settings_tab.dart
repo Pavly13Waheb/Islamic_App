49a7f65217ba5d7e00e26dbb144527a78b78ae90
Pavly Waheb <130704865+Pavly13Waheb@users.noreply.github.com>
2023-07-15 01:28:04 +0300

    Finished app

--- lib/screens/navbarscreens/settings_tab.dart
+++ lib/screens/navbarscreens/settings_tab.dart
@@ -15,113 +15,122 @@ class _SettingsTabState extends State<SettingsTab> {
   @override
   Widget build(BuildContext context) {
     SettingsProvider provider = Provider.of(context);
-    var language = provider.currentLocale;
-    return Container(
-      padding: EdgeInsets.only(
-          top: MediaQuery.of(context).size.height * 0.16, left: 5, right: 5),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Icon(Icons.settings, size: 50, color: AppColor.secColor),
-          Text(
-              textAlign: TextAlign.start,
-              AppLocalizations.of(context)!.language,
-              style: TextStyle(
-                  color: AppColor.primColor,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold)),
-          InkWell(
-            onTap: () {},
-            child: Container(
-              height: 40,
-              padding:
-                  EdgeInsetsDirectional.symmetric(horizontal: 15, vertical: 5),
-              margin: EdgeInsets.only(top: 10, bottom: 10),
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.all(Radius.circular(20)),
-                  border: Border.all(color: AppColor.secColor)),
-              child: DropdownButtonHideUnderline(
-                  child: DropdownButton(
-                elevation: 0,
-                alignment: Alignment.center,
-                items: [
-                  DropdownMenuItem(
-                    value: "en",
-                    child: Text("English",
-                        style: Theme.of(context).textTheme.bodyLarge),
-                    onTap: () {
-                      provider.currentLocale = "en";
-                      provider.notifyListeners();
-                    },
-                  ),
-                  DropdownMenuItem(
-                    value: "ar",
-                    child: Text("العربية",
-                        style: Theme.of(context).textTheme.bodyLarge),
-                    onTap: () {
-                      provider.currentLocale = "ar";
-                      provider.notifyListeners();
-                    },
-                  )
-                ],
-                onChanged: (value) {
-                  setState(() {
-                    language = value!;
-                  });
-                },
-                value: language,
-              )),
-            ),
+    String? language = provider.currentLocale;
+    ThemeMode? mod = provider.currentTheme;
+    return ChangeNotifierProvider(
+      create: (context) => provider,
+      builder: (context, child) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(AppLocalizations.of(context)!.setting),
           ),
-          Text(
-              textAlign: TextAlign.start,
-              AppLocalizations.of(context)!.mod,
-              style: TextStyle(
-                  color: AppColor.primColor,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold)),
-          InkWell(
-            onTap: () {},
-            child: Container(
-              height: 40,
-              padding:
-                  EdgeInsetsDirectional.symmetric(horizontal: 15, vertical: 5),
-              margin: EdgeInsets.only(top: 10, bottom: 10),
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.all(Radius.circular(20)),
-                  border: Border.all(color: AppColor.secColor)),
-              child: DropdownButtonHideUnderline(
-                child: DropdownButton<String>(
-                  elevation: 0,
-                  alignment: Alignment.center,
-                  items: [
-                    AppLocalizations.of(context)!.light,
-                    AppLocalizations.of(context)!.dark
-                  ]
-                      .map(
-                        (e) => DropdownMenuItem<String>(
-                          value: e,
-                          child: Text(e,
-                              style: TextStyle(
-                                  color: AppColor.primColor,
-                                  fontWeight: FontWeight.bold)),
-                        ),
+          body: Container(
+            padding: EdgeInsets.only(
+                top: MediaQuery.of(context).size.height * 0.16,
+                left: 5,
+                right: 5),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Icon(Icons.settings, size: 50, color: AppColor.primColor),
+                Text(
+                    textAlign: TextAlign.start,
+                    AppLocalizations.of(context)!.language,
+                    style: Theme.of(context).textTheme.bodyMedium),
+                Container(
+                  height: 40,
+                  padding: const EdgeInsetsDirectional.symmetric(
+                      horizontal: 15, vertical: 5),
+                  margin: const EdgeInsets.only(top: 10, bottom: 10),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: const BorderRadius.all(Radius.circular(20)),
+                      border: Border.all(color: AppColor.primColor)),
+                  child: DropdownButtonHideUnderline(
+                      child: DropdownButton(
+                    elevation: 0,
+                    alignment: Alignment.center,
+                    items: [
+                      DropdownMenuItem(
+                        value: "en",
+                        child: Text("English",
+                            style: Theme.of(context).textTheme.bodySmall),
+                        onTap: () async {
+                          provider.currentLocale = "en";
+                          provider.notifyListeners();
+                        },
+                      ),
+                      DropdownMenuItem(
+                        value: "ar",
+                        child: Text("العربية",
+                            style: Theme.of(context).textTheme.bodySmall),
+                        onTap: () async {
+                          provider.currentLocale = "ar";
+                          provider.notifyListeners();
+                        },
                       )
-                      .toList(),
-                  onChanged: (value) {
-                    setState(() {
-                      mod = value;
-                    });
-                  },
-                  value: mod,
+                    ],
+                    onChanged: (value) {
+                      // setState(() {
+                      //   language = value;
+                      // });
+                    },
+                    value: language,
+                  )),
                 ),
-              ),
+                Text(
+                    textAlign: TextAlign.start,
+                    AppLocalizations.of(context)!.mod,
+                    style: Theme.of(context).textTheme.bodyMedium),
+                Container(
+                  height: 40,
+                  padding: const EdgeInsetsDirectional.symmetric(
+                      horizontal: 15, vertical: 5),
+                  margin: const EdgeInsets.only(top: 10, bottom: 10),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: const BorderRadius.all(Radius.circular(20)),
+                      border: Border.all(color: AppColor.primColor)),
+                  child: DropdownButtonHideUnderline(
+                    child: DropdownButton(
+                      elevation: 0,
+                      alignment: Alignment.center,
+                      items: [
+                        DropdownMenuItem(
+                          value: ThemeMode.light,
+                          child: Text(AppLocalizations.of(context)!.light,
+                              style: Theme.of(context).textTheme.bodySmall),
+                          onTap: () {
+                            provider.currentTheme = ThemeMode.light;
+                            provider.notifyListeners();
+                          },
+                        ),
+                        DropdownMenuItem(
+                          value: ThemeMode.dark,
+                          child: Text(AppLocalizations.of(context)!.dark,
+                              style: Theme.of(context).textTheme.bodySmall),
+                          onTap: () {
+                            provider.currentTheme = ThemeMode.dark;
+                            provider.notifyListeners();
+                          },
+                        ),
+                      ],
+                      onChanged: (value) {
+                        // setState(
+                        //       () {
+                        //     mod = value;
+                        //   },
+                        // );
+                      },
+                      value: mod,
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
